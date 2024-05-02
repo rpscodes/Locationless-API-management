8547eb3536ed1a7c756fff624908f5c0978d99e9
Vamsi Ravula <83864467+rpscodes@users.noreply.github.com>
2024-05-02 14:56:39 -0700

    Update label_services.sh
    Show standard errors

--- label_services.sh
+++ label_services.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Annotate quarkus-openapi service
-oc annotate svc/quarkus-openapi "discovery.3scale.net/description-path=/openapi?format=json" >/dev/null 2>&1
-oc annotate svc/quarkus-openapi discovery.3scale.net/port="8080" >/dev/null 2>&1
-oc annotate svc/quarkus-openapi discovery.3scale.net/scheme=http >/dev/null 2>&1
-oc label svc/quarkus-openapi discovery.3scale.net="true" >/dev/null 2>&1
+oc annotate svc/quarkus-openapi "discovery.3scale.net/description-path=/openapi?format=json" >/dev/null
+oc annotate svc/quarkus-openapi discovery.3scale.net/port="8080" >/dev/null
+oc annotate svc/quarkus-openapi discovery.3scale.net/scheme=http >/dev/null
+oc label svc/quarkus-openapi discovery.3scale.net="true" >/dev/null
 
 # Annotate nodejs-api service
-oc annotate svc/nodejs-api discovery.3scale.net/port="8080" >/dev/null 2>&1
-oc annotate svc/nodejs-api discovery.3scale.net/scheme=http >/dev/null 2>&1
-oc label svc/nodejs-api discovery.3scale.net="true" >/dev/null 2>&1
+oc annotate svc/nodejs-api discovery.3scale.net/port="8080" >/dev/null
+oc annotate svc/nodejs-api discovery.3scale.net/scheme=http >/dev/null
+oc label svc/nodejs-api discovery.3scale.net="true" >/dev/null
 
 # Add cluster role to user
-oc adm policy add-cluster-role-to-user view system:serviceaccount:3scale:amp >/dev/null 2>&1
+oc adm policy add-cluster-role-to-user view system:serviceaccount:3scale:amp >/dev/null
 
 echo "Service label complete"
